04c37b37333eb7b6ca50410de01fbc0147052fe9
HyperbolicStudios <markinoe@gmail.com>
2022-10-12 18:40:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+# create app
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return("Hello World!")
+def home():
+    return("Online!")
+
+if __name__ == '__main__':
+    app.run()
